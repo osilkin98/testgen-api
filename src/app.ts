f75e76289b5f67d94f2e4a0b39704fea2842b30b
Oleg Silkin <16809287+osilkin98@users.noreply.github.com>
2021-10-25 09:04:44 -0400

    Adds ignores + docker + types + setup
    Signed-off-by: Oleg Silkin <16809287+osilkin98@users.noreply.github.com>

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import express, {Request, Application, Response } from 'express'
+
+// setup the express app
+const app: Application = express()
+app.use(express.json())
+
+// // knative hello world
+// app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  // console.log('/ received a request')
+  // 
+  // const target: string = process.env.TARGET || 'World'
+  // res.send(`Hello ${target}!\n`)
+// })
+
+
+// POST request with 
+app.post('/test', (req: Request, res: Response) => {
+  console.log(req.body)
+  
+  res.sendStatus(200)
+})
+
+
+const port: string = process.env.PORT || '5000'
+const portNum: number = parseInt(port)
+app.listen(
+  portNum, () => console.log(`Hello world is listening on port ${portNum}\n`)
+)
---
+++ src/types.ts
@@ -0,0 +1,10 @@
+// types
+
+// type to represent test data body
+type ClientCodeData = {
+  code: string,
+  language: string, // enumerate this
+  testDescription: string
+}
+
+export { ClientCodeData };
